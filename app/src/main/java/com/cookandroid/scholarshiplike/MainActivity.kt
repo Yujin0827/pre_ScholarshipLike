fcc02c5045ac04d218b3df5886800b8accd55086
Yujin0827 <yjjeon980827@gmail.com>
2021-05-07 11:57:02 +0900

    하단바 생성

--- app/src/main/java/com/cookandroid/scholarshiplike/MainActivity.kt
+++ app/src/main/java/com/cookandroid/scholarshiplike/MainActivity.kt
@@ -1,44 +1,42 @@
 package com.cookandroid.scholarshiplike
 
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 open class MainActivity : AppCompatActivity() {
+
+    //탭 화면 변수 생성
+    private val hometab = HomeTabActivity()
+    private val scholartab = ScholarshipTabActivity()
+    private val magazinetab = MagazineTabActivity()
+    private val profiletab = ProfileTabActivity()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setFrag(0) // 첫 화면 = homeTabActivity
-
-        boolean onNavigationItemSelected(MenuItem menuItem) {
-            switch (menuItem.getItemId()) {
-
-            }
-            return true;
-        }
+        initNavigationBar()
 
     }
 
-    //하단바 교체가 일어나는 실행문
-    private fun setFrag(fragNum : Int) {
-        val ft = supportFragmentManager.beginTransaction()
-
-        when(fragNum)
-        {
-            0 -> {
-                ft.replace(R.id.nav, HomeTabActivity()).commit()
-            }
-            1 -> {
-                ft.replace(R.id.nav, ScholarshipTabActivity()).commit()
-            }
-            2 -> {
-                ft.replace(R.id.nav, MagazineTabActivity()).commit()
-            }
-            3 -> {
-                ft.replace(R.id.nav, ProfileTabActivity()).commit()
+    private fun initNavigationBar() {
+        tabNav.run {
+            setOnNavigationItemSelectedListener {
+                when(it.itemId) {
+                    R.id.homeTab -> {
+                        changeFragment(hometab)
+                    }
+                }
+                true
             }
         }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.nav, fragment).commit()
     }
 }
