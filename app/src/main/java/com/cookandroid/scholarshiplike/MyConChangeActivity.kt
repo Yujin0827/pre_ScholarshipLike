052ae36425ab131b8bb6de3de7c04b9318983c7b
kyunghee <kyunghee1210@naver.com>
2021-05-14 11:44:32 +0900

    내 조건 수정 페이지 생성

---
+++ app/src/main/java/com/cookandroid/scholarshiplike/MyConChangeActivity.kt
@@ -0,0 +1,12 @@
+package com.cookandroid.scholarshiplike
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyConChangeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.my_con_change)
+    }
+}
