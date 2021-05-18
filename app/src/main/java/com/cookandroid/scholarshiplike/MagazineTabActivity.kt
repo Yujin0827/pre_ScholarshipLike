689d491812bd2ea3b638c9fe86a27ee7eb7238d5
immindi <rlaalswl2407@gmail.com>
2021-05-18 18:55:59 +0900

    리사이클러뷰(파이어스토어X)

--- app/src/main/java/com/cookandroid/scholarshiplike/MagazineTabActivity.kt
+++ app/src/main/java/com/cookandroid/scholarshiplike/MagazineTabActivity.kt
@@ -1,17 +1,41 @@
 package com.cookandroid.scholarshiplike
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.magazinetab.*
+import kotlinx.android.synthetic.main.magazinetab_item.view.*
+
 class MagazineTabActivity : Fragment() {
+
+    // RecyclerView.adapter에 지정할 Adapter
+    private lateinit var magazineAdapter: MagazineTabAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.magazinetab, container, false)
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //메인 액티비티의 데이터를 프래그먼트로 넘겨받음
+        var list: ArrayList<TestData> = requireActivity().intent!!.extras!!.get("DataList") as ArrayList<TestData>
+        Log.e("FirstFragment", "Data List: ${list}")
+
+        // Fragment에서 전달받은 list를 넘기면서 ListAdapter 생성
+        magazineAdapter = MagazineTabAdapter(list)
+        MagazineRecyclerView.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+        // RecyclerView.adapter에 지정
+        MagazineRecyclerView.adapter = magazineAdapter
     }
 }
--- app/src/main/java/com/cookandroid/scholarshiplike/MainActivity.kt
+++ app/src/main/java/com/cookandroid/scholarshiplike/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.cookandroid.scholarshiplike
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.magazinetab_item.view.*
+
 
 open class MainActivity : AppCompatActivity() {
 
@@ -15,7 +20,6 @@ open class MainActivity : AppCompatActivity() {
     private val scholarshiptab = ScholarshipTabActivity()
     private val magazinetab = MagazineTabActivity()
     private val profiletab = ProfileTabActivity()
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +35,8 @@ open class MainActivity : AppCompatActivity() {
 
     }
 
+
+
     //하단바 누르면 탭 화면 전환
     private fun initNavigationBar() {
         tabNav.run {
@@ -44,6 +50,8 @@ open class MainActivity : AppCompatActivity() {
                     }
                     R.id.magazineTab -> {
                         changeFragment(magazinetab)
+                        //메인 액티비티 데이터를 프래그먼트로 넘겨줌
+                        intent.putExtra("DataList", dataList)
                     }
                     R.id.profileTab -> {
                         changeFragment(profiletab)
@@ -61,4 +69,49 @@ open class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction().replace(R.id.nav, fragment).commit()
     }
 
+    //매거진 임시 데이터
+    var dataList: ArrayList<TestData> = arrayListOf(
+        TestData("첫 번째 데이터1", "두 번째 데이터1", "세 번째 데이터1"),
+        TestData("첫 번째 데이터2", "두 번째 데이터2", "세 번째 데이터2"),
+        TestData("첫 번째 데이터3", "두 번째 데이터3", "세 번째 데이터3"),
+        TestData("첫 번째 데이터4", "두 번째 데이터4", "세 번째 데이터4"),
+        TestData("첫 번째 데이터5", "두 번째 데이터5", "세 번째 데이터5"),
+        TestData("첫 번째 데이터6", "두 번째 데이터6", "세 번째 데이터6"),
+        TestData("첫 번째 데이터7", "두 번째 데이터7", "세 번째 데이터7"),
+        TestData("첫 번째 데이터8", "두 번째 데이터8", "세 번째 데이터8"),
+        TestData("첫 번째 데이터9", "두 번째 데이터9", "세 번째 데이터9"),
+        TestData("첫 번째 데이터10", "두 번째 데이터10", "세 번째 데이터10"),
+        TestData("첫 번째 데이터11", "두 번째 데이터11", "세 번째 데이터11"),
+        TestData("첫 번째 데이터12", "두 번째 데이터12", "세 번째 데이터12"),
+        TestData("첫 번째 데이터13", "두 번째 데이터13", "세 번째 데이터13"),
+        TestData("첫 번째 데이터14", "두 번째 데이터14", "세 번째 데이터14"),
+        TestData("첫 번째 데이터15", "두 번째 데이터15", "세 번째 데이터15")
+    )
+
+}
+
+//매거진 테스트 데이터 클래스
+class TestData(
+    private var data1: String? = null,
+    private var data2: String? = null,
+    private var data3: String? = null
+){
+    fun getData1(): String? {
+        return data1
+    }
+    fun setData1(name: String) {
+        this.data1 = data1
+    }
+    fun getData2(): String? {
+        return data2
+    }
+    fun setData2(address: String) {
+        this.data2 = data2
+    }
+    fun getData3(): String? {
+        return data3
+    }
+    fun setData3(type: String) {
+        this.data3 = data3
+    }
 }
