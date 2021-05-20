7b0a336aa21522633d2d0942bb5ac6805606475c
immindi <rlaalswl2407@gmail.com>
2021-05-20 17:41:53 +0900

    파이어스토어 추가/데이터생성 메인>프래그먼트/데이터갱신X

--- app/src/main/java/com/cookandroid/scholarshiplike/MagazineTabActivity.kt
+++ app/src/main/java/com/cookandroid/scholarshiplike/MagazineTabActivity.kt
@@ -1,26 +1,32 @@
 package com.cookandroid.scholarshiplike
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
-
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.*
 import kotlinx.android.synthetic.main.magazinetab.*
-import kotlinx.android.synthetic.main.magazinetab_item.view.*
 
 class MagazineTabActivity : Fragment() {
 
-    private lateinit var magazineAdapter: MagazineTabAdapter // 리사이클러뷰 어댑터
-
+    private var firestore : FirebaseFirestore? = null// Firestore 인스턴스
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.magazinetab, container, false)
+
+//            // ArrayList 비워줌
+//            postList?.clear()
+//
+//            for (snapshot in querySnapshot!!.documents) {
+//                var item = snapshot.toObject(Post::class.java)
+//                postList?.add(item!!)
+//            }
+//        }
 
         return view
     }
@@ -28,15 +34,31 @@ class MagazineTabActivity : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //메인 액티비티의 데이터를 프래그먼트로 넘겨받음
-        var list: ArrayList<TestData> = requireActivity().intent!!.extras!!.get("DataList") as ArrayList<TestData>
-        Log.e("FirstFragment", "Data List: ${list}")
+        firestore = FirebaseFirestore.getInstance() // Firestore 인스턴스 초기화
+        var postList : ArrayList<Post> = arrayListOf()
 
-        // Fragment에서 전달받은 list를 넘기면서 ListAdapter 생성
-        magazineAdapter = MagazineTabAdapter(list)
-        MagazineRecyclerView.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+        firestore?.collection("장학라이크")?.document("매거진")?.collection("금융")?.get()?.addOnSuccessListener { result ->
+            // 성공할 경우
+            postList?.clear()
 
-        // RecyclerView.adapter에 지정
-        MagazineRecyclerView.adapter = magazineAdapter
+            for (document in result) {  // 가져온 문서들은 result에 들어감
+                val item = Post(document["title"] as String)
+                postList?.add(item)
+                Log.d("postList",item.toString())
+            }
+        }
+
+        magazinerecyclerView.layoutManager = LinearLayoutManager(requireActivity(), LinearLayoutManager.VERTICAL, false)
+        magazinerecyclerView.setHasFixedSize(true) //리사이클러뷰 성능 개선 방안
+
+        magazinerecyclerView.adapter = MagazineTabAdapter(postList)
+
+//        val postList = arrayListOf(
+//            Post("대학생에게 추천하는 체크카드 TOP10"),
+//            Post("자취방 전기세 아끼는 법"),
+//            Post("소비를 줄여주는 좋은 습관들"),
+//            Post("대학 졸업까지 1억 모으기"),
+//            Post("안녕하세요 김민지입니당")
+//        )
     }
 }
--- app/src/main/java/com/cookandroid/scholarshiplike/MainActivity.kt
+++ app/src/main/java/com/cookandroid/scholarshiplike/MainActivity.kt
@@ -35,8 +35,6 @@ open class MainActivity : AppCompatActivity() {
 
     }
 
-
-
     //하단바 누르면 탭 화면 전환
     private fun initNavigationBar() {
         tabNav.run {
@@ -50,8 +48,6 @@ open class MainActivity : AppCompatActivity() {
                     }
                     R.id.magazineTab -> {
                         changeFragment(magazinetab)
-                        //메인 액티비티 데이터를 프래그먼트로 넘겨줌
-                        intent.putExtra("DataList", dataList)
                     }
                     R.id.profileTab -> {
                         changeFragment(profiletab)
@@ -69,49 +65,4 @@ open class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction().replace(R.id.nav, fragment).commit()
     }
 
-    //매거진 임시 데이터
-    var dataList: ArrayList<TestData> = arrayListOf(
-        TestData("첫 번째 데이터1", "두 번째 데이터1", "세 번째 데이터1"),
-        TestData("첫 번째 데이터2", "두 번째 데이터2", "세 번째 데이터2"),
-        TestData("첫 번째 데이터3", "두 번째 데이터3", "세 번째 데이터3"),
-        TestData("첫 번째 데이터4", "두 번째 데이터4", "세 번째 데이터4"),
-        TestData("첫 번째 데이터5", "두 번째 데이터5", "세 번째 데이터5"),
-        TestData("첫 번째 데이터6", "두 번째 데이터6", "세 번째 데이터6"),
-        TestData("첫 번째 데이터7", "두 번째 데이터7", "세 번째 데이터7"),
-        TestData("첫 번째 데이터8", "두 번째 데이터8", "세 번째 데이터8"),
-        TestData("첫 번째 데이터9", "두 번째 데이터9", "세 번째 데이터9"),
-        TestData("첫 번째 데이터10", "두 번째 데이터10", "세 번째 데이터10"),
-        TestData("첫 번째 데이터11", "두 번째 데이터11", "세 번째 데이터11"),
-        TestData("첫 번째 데이터12", "두 번째 데이터12", "세 번째 데이터12"),
-        TestData("첫 번째 데이터13", "두 번째 데이터13", "세 번째 데이터13"),
-        TestData("첫 번째 데이터14", "두 번째 데이터14", "세 번째 데이터14"),
-        TestData("첫 번째 데이터15", "두 번째 데이터15", "세 번째 데이터15")
-    )
-
-}
-
-//매거진 테스트 데이터 클래스
-class TestData(
-    private var data1: String? = null,
-    private var data2: String? = null,
-    private var data3: String? = null
-){
-    fun getData1(): String? {
-        return data1
-    }
-    fun setData1(name: String) {
-        this.data1 = data1
-    }
-    fun getData2(): String? {
-        return data2
-    }
-    fun setData2(address: String) {
-        this.data2 = data2
-    }
-    fun getData3(): String? {
-        return data3
-    }
-    fun setData3(type: String) {
-        this.data3 = data3
-    }
 }
