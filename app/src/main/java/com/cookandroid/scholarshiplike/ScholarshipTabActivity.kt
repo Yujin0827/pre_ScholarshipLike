cf05f0c3898d302ec983a82ebc051788f6e9db92
skdms8827 <skdms8827@naver.com>
2021-05-24 21:20:23 +0900

    중첩 프래그먼트 완성

--- app/src/main/java/com/cookandroid/scholarshiplike/ScholarshipTabActivity.kt
+++ app/src/main/java/com/cookandroid/scholarshiplike/ScholarshipTabActivity.kt
@@ -1,35 +1,53 @@
 package com.cookandroid.scholarshiplike
 
-import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.scholarshiptab.*
+import androidx.fragment.app.FragmentTransaction
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.fragment_korea_scholar.*
+import kotlinx.android.synthetic.main.scholarshiptab.*
+
+
 
 class ScholarshipTabActivity : Fragment() {
 
-   //lateinit var context : Context
+    lateinit var like : ImageView       // 좋아요 페이지로 이동 버튼
+    lateinit var alarm : ImageView      // 알람 페이지로 이동 버튼
+    lateinit var myScholarButton: Button // 내 장학금 버튼
+    lateinit var koreaScholarButton: Button // 전체 장학금 버튼
+    private var mf = MyScholarFragment()
+    private var kf = KoreaScholarFragment()
+
+
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.scholarshiptab, container, false)
 
 
+        like = view.findViewById<ImageView>(R.id.like)              // hometab의 좋아요 버튼 변수 생성
+        alarm = view.findViewById<ImageView>(R.id.alarm)            // hometab의 알람 버튼 변수 생성
+        myScholarButton = view.findViewById<Button>(R.id.myScholarBt)
+        koreaScholarButton = view.findViewById<Button>(R.id.koreaScholarBt)
 
-        fun onCreate(savedInstanceState: Bundle?)
-        {
-            super.onCreate(savedInstanceState)
-     //       setContentView(R.layout.scholarshiptab)
 
 
-       //     val adapter = MyPagerAdapter(supportFragmentManager)
-         //   adapter.addFragment(FragmentMyScholar(), "내 장학금")
-           // adapter.addFragment(FragmentKoreaScholar(), "전체 장학금")
 
-            //cholarShipViewpager.adapter = adapter
-            scholarShipTabBar.setupWithViewPager(scholarShipViewpager)
+
+
+        myScholarButton.setOnClickListener{
+            setFrag(mf)
+        }
+        koreaScholarButton.setOnClickListener{
+            setFrag(kf)
         }
 
         return view
@@ -38,4 +56,23 @@ class ScholarshipTabActivity : Fragment() {
 
 
 
+
+    private fun setFrag(fragment : Fragment){
+        childFragmentManager.beginTransaction().replace(R.id.scholarFram, fragment).commit()
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+    }
+
+
+
+
+
 }
+
+
+
