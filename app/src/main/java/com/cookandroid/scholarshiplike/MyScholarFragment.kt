2d3e15f07f20be5941a46e62432d3ec580f2b856
skdms8827 <skdms8827@naver.com>
2021-05-25 20:46:11 +0900

    다시 머지ㅎㅎ

---
+++ app/src/main/java/com/cookandroid/scholarshiplike/MyScholarFragment.kt
@@ -0,0 +1,24 @@
+package com.cookandroid.scholarshiplike
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MyScholarFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_scholar, container, false)
+    }
+
+
+}
--- app/src/main/java/com/cookandroid/scholarshiplike/ScholarshipTabActivity.kt
+++ app/src/main/java/com/cookandroid/scholarshiplike/ScholarshipTabActivity.kt
@@ -23,10 +23,8 @@ class ScholarshipTabActivity : Fragment() {
     lateinit var alarm : ImageView      // 알람 페이지로 이동 버튼
     lateinit var myScholarButton: Button // 내 장학금 버튼
     lateinit var koreaScholarButton: Button // 전체 장학금 버튼
-    private var mf = FragmentMyScholar()
-    private var kf = FragmentKoreaScholar()
-
-
+    private var mf = FragmentMyScholar() // 내장학금 프래그먼트
+    private var kf = FragmentKoreaScholar() // 전체 장학금 프래그먼트
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -35,18 +33,16 @@ class ScholarshipTabActivity : Fragment() {
 
         like = view.findViewById<ImageView>(R.id.like)              // hometab의 좋아요 버튼 변수 생성
         alarm = view.findViewById<ImageView>(R.id.alarm)            // hometab의 알람 버튼 변수 생성
-        myScholarButton = view.findViewById<Button>(R.id.myScholarBt)
-        koreaScholarButton = view.findViewById<Button>(R.id.koreaScholarBt)
+        myScholarButton = view.findViewById<Button>(R.id.myScholarBt) // 내 장학금 버튼 변수 생성
+        koreaScholarButton = view.findViewById<Button>(R.id.koreaScholarBt) //전체 장학금 버튼 변수 생성
+
+        setFrag(mf)
 
 
-
-
-
-
-        myScholarButton.setOnClickListener{
+        myScholarButton.setOnClickListener{ // 내장학금 버튼 이벤트
             setFrag(mf)
         }
-        koreaScholarButton.setOnClickListener{
+        koreaScholarButton.setOnClickListener{ // 전체장학금 버튼 이벤트
             setFrag(kf)
         }
 
@@ -57,7 +53,7 @@ class ScholarshipTabActivity : Fragment() {
 
 
 
-    private fun setFrag(fragment : Fragment){
+    private fun setFrag(fragment : Fragment){ // 자식프래그먼트 생성
         childFragmentManager.beginTransaction().replace(R.id.scholarFram, fragment).commit()
 
     }
@@ -67,12 +63,22 @@ class ScholarshipTabActivity : Fragment() {
 
 
     }
+    // 프래그먼트 생성시 툴바 hide
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
+    }
+
+    // 프래그먼트 종료시 툴바 show
+    override fun onStop() {
+        super.onStop()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.show()
+    }
 
 
 
 
 
 }
-
 
 
